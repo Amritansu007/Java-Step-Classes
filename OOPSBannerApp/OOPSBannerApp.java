416978e24621a88a7600939eb25c41eea80834fe
Amritansu007 <2006amritansusingh@gmail.com>
2026-03-15 22:51:42 +0530

    UC3

---
+++ OOPSBannerApp/OOPSBannerApp.java
@@ -0,0 +1,28 @@
+package OOPSBannerApp;
+
+public class OOPSBannerApp {
+	public static void main(String[] args) {
+		System.out.println(String.join(" ", 
+        " *** ", " *** ", " **** ", " ****"));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*   *", "*    "));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*   *", "*    "));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", " **** ", " *** "));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*     ", "    *"));
+
+System.out.println(String.join(" ", 
+        "*   *", "*   *", "*     ", "    *"));
+
+System.out.println(String.join(" ", 
+        " *** ", " *** ", "*     ", " ****"));
+	}
+}
+
+
